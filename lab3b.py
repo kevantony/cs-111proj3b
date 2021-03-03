3e6404cc6aa7f7703077f64d6ea3bb03552cf489
Kevin J Antony <classkan@lnxsrv09.seas.ucla.edu>
2021-03-03 14:55:40 -0800

    Update

--- lab3b.py
+++ lab3b.py
@@ -4,15 +4,41 @@ import csv
 
 
 
+class DIRENT:
+    def __init__(self, row):
+        self.parent_inode_number = int(row[1])
+        self.logical_byte_offset = int(row[2])
+        self.inode_number = int(row[3])
+        self.entry_length = int(row[4])
+        self.name_length = int(row[5])
+        self.name = row[6]
+
 
 def main():
     inconsistencies_found = false
-
+    free_blocks = []
+    free_inodes = []
     try:
         with open(file) as csvfile:
-            for row
+            for row in csvfile:
+                if row[0] == 'SUPERBLOCK':
+                    superblock = SUPERBLOCK(row)
+                elif row[0] == 'GROUP':
+                    gorup = Group(row)
+                elif row[0] = 'BFREE':
+                    free_blocks.append(int(row[1]))
+                elif row[0] = 'IFREE':
+                    free_inodes.append(int(row[1])) #IRFREE action
+                elif row[0] = 'INODE':
+                    #INODE action
+                elif row[0] = 'DIRENT':
+                    DIRENT(row)#DIRENT action
+                elif row[0] = 'INDIRECT':
+                    #INDIRECT action
+                else:
+                    #Invalid option
 
-
+                
     except:
         exit(1) #might need to do some other stuff instead
 
@@ -20,9 +46,9 @@ def main():
 
 
     if inconsistencies_found:
-        exit(2)
+        sys.exit(2)
 
-    exit(0)
+    sys.exit(0)
 
 if __name__ == '__main__':
     main()
