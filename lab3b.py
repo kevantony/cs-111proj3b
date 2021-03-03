86636e5dc0b9794d7640b39e9a711eb1725ec298
Shashank Sreehari Venkat <classsve@lnxsrv09.seas.ucla.edu>
2021-03-03 14:58:23 -0800

    superblock and group classes

--- lab3b.py
+++ lab3b.py
@@ -1,7 +1,27 @@
 import sys
 import csv
 
+class SuperBlock:
+    """Creates a class for the information relating to the superblock."""
+    def __init__(self, row):
+        self.num_of_blocks = int(row[1])
+        self.num_of_inodes = int(row[2])
+        self.block_size = int(row[3])
+        self.inode_size = int(row[4])
+        self.blocks_per_group = int(row[5])
+        self.inode_per_group = int(row[6])
+        self.first_nonres_inode = int(row[7])
 
+class Group:
+    """Creates a class for the group info."""
+    def __init__(self, row):
+        self.total_num_blocks_in_group = int(row[2])
+        self.total_num_inodes_in_group = int(row[3])
+        self.num_free_blocks = int(row[4])
+        self.num_free_inodes = int(row[5])
+        self.blocknum_of_block_bitmap = int(row[6])
+        self.blocknum_of_inode_bitmap = int(row[7])
+        self.blocknum_of_first_inode_blocks = int(row[8])
 
 
 class DIRENT:
