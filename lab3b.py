902c4a192074f7698de7623d12cc53f168a01ab2
Kevin J Antony <classkan@lnxsrv09.seas.ucla.edu>
2021-03-03 21:03:29 -0800

    Indirect class

--- lab3b.py
+++ lab3b.py
@@ -33,11 +33,37 @@ class DIRENT:
         self.name_length = int(row[5])
         self.name = row[6]
 
+class INDIRECT:
+    def __init__(self.row):
+        self.parent_inode_number = int(row[1])
+        self.indirextion = int(row[2])
+        self.logical_block_offset = int(row[3])
+        self.indierct_block_number = int(row[4])
+        self.reference_block_number = int(row[5])
+
+
+class inodes:
+    def __init__(self, row):
+        self.inode_number = int(row[1])
+        self.file_type = row[2]
+        self.mode = int(row[3])
+        self.owner = int(row[4])
+        self.group = int(row[5])
+        self.link_count = int(row[6])
+        self.last_change = row[7]
+        self.modification_time = row[8]
+        self.last_access = row[9]
+        self.file_size = int(row[10])
+        self.block_space = int(row[11])
+
+
 
 def main():
     inconsistencies_found = false
     free_blocks = []
     free_inodes = []
+    inodes = []
+
     try:
         with open(file) as csvfile:
             for row in csvfile:
@@ -60,10 +86,11 @@ def main():
 
                 
     except:
-        exit(1) #might need to do some other stuff instead
+        sys.exit(1) #might need to do some other stuff instead
 
 
-
+    if gorup.num_free_blocks != (group.total_num_blocks_in_group - len(free_blocks)):
+        print("")
 
     if inconsistencies_found:
         sys.exit(2)
